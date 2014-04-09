a187829c60d4656cbc6db1ebec8f1f08166d6290
Reika <reikasminecraft@gmail.com>
2014-04-08 20:38:36 -0400

    Update

--- Overrides/BlockClosedEndPortal.java
+++ Overrides/BlockClosedEndPortal.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * @author Reika Kalseki
+ * 
+ * Copyright 2014
+ * 
+ * All rights reserved.
+ * Distribution of the software in any form is only allowed with
+ * explicit, prior permission from the owner.
+ ******************************************************************************/
 package Reika.LegacyCraft.Overrides;
 
 import net.minecraft.block.BlockEndPortal;
